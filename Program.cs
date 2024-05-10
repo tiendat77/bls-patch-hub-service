4733ad4162a073b37745615a76c05e343c10e57c
Binh Tran <binhtran@blogicsystems.com>
2024-05-10 10:51:14 +0700

    feat: ✨ add serilog

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService(options =>
@@ -19,5 +22,14 @@ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 builder.Services.AddSingleton<UpdateService>();
 builder.Services.AddHostedService<WindowsBackgroundService>();
 
+builder.Services.AddLogging(configure => {
+    var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", LogEventLevel.Error, "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 365)
+    .WriteTo.Console(LogEventLevel.Warning, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+    configure.AddSerilog(logger);
+});
 IHost host = builder.Build();
-host.Run();
+host.Run(); 

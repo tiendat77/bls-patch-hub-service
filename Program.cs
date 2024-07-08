eae80830391a2bf3a2c5277895dd680977845205
tuanph-pc-2 <tuanpham@blogicsystems.com>
2024-07-08 19:09:11 +0700

    fix: Initialize Serilog for file logging and configure Windows Service with custom options

--- Program.cs
+++ Program.cs
@@ -1,36 +1,38 @@
-﻿using App;
-
-using System.Runtime.InteropServices;
+using App;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
-using Serilog;
 using Serilog.Events;
+using Serilog;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddWindowsService(options =>
+internal class Program
 {
-    options.ServiceName = "PatchHub Service";
-});
+    static void Main(string[] args)
+    {
 
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-{
-    LoggerProviderOptions
-        .RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+       
+        Log.Logger = new LoggerConfiguration()
+               .WriteTo.File(@"C:/Users/blogic/source/repos/PathService/release/Logs/log.txt", LogEventLevel.Information, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 365)
+               .CreateLogger();
+
+
+        builder.Logging.Services.AddSerilog(Log.Logger);
+
+
+        builder.Services.AddWindowsService(options =>
+        {
+            options.ServiceName = "Demo Report Service 222222";
+        });
+
+        LoggerProviderOptions.RegisterProviderOptions<
+            EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+        builder.Services.AddSingleton<UpdateService>();
+        builder.Services.AddHostedService<WindowsBackgroundService>();
+
+        IHost host = builder.Build();
+
+        host.Run();
+    }
 }
-
-builder.Services.AddSingleton<UpdateService>();
-builder.Services.AddHostedService<WindowsBackgroundService>();
-
-builder.Services.AddLogging(configure => {
-    var ConsoleLogLevel = builder.Configuration.GetSection("Serilog:ConsoleLogLevel").Get<LogEventLevel>();
-    var FileLogLevel = builder.Configuration.GetSection("Serilog:FileLogLevel").Get<LogEventLevel>();
-
-    var logger = new LoggerConfiguration()
-    .WriteTo.Console(ConsoleLogLevel, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}")
-    .WriteTo.File("Logs/log.txt", FileLogLevel, "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 365)
-    .CreateLogger();
-
-    configure.AddSerilog(logger);
-});
-IHost host = builder.Build();
-host.Run(); 
